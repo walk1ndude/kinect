52b908a4319da5da5ca30df80e6f785b1385d3df
walk1ndude <lunarstrainut@gmail.com>
2014-04-11 20:06:58 +0400

    clean main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <QtGui/QGuiApplication>
-<<<<<<< HEAD
 #include <QtCore/QThread>
 #include <QtCore/QDebug>
 
@@ -23,38 +22,4 @@ int main(int argc, char * argv[]) {
     kinectThread.start();
 
     return app.exec();
-=======
-
-#include <QtQml/QQmlApplicationEngine>
-
-#include "dicomreader.h"
-#include "glviewer.h"
-
-int main(int argc, char * argv[]) {
-    QGuiApplication a(argc, argv);
-
-    QQmlApplicationEngine engine(QUrl("qrc:/qml/MainWindow.qml"));
-    QObject * appWindow = engine.rootObjects().at(0);
-
-    DicomReader dicomReader;
-
-    QSurfaceFormat format;
-    format.setSamples(16);
-    format.setVersion(3, 3);
-    format.setRenderableType(QSurfaceFormat::OpenGL);
-    format.setProfile(QSurfaceFormat::CoreProfile);
-
-    GLviewer glviewer(format);
-
-    glviewer.resize(640, 640);
-
-    QObject::connect(appWindow, SIGNAL(fileOpened(QString)), &dicomReader, SLOT(readFile(QString)));
-    QObject::connect(appWindow, SIGNAL(sliceNumberChanged(int)), &dicomReader, SLOT(changeSliceNumber(int)));
-
-    QObject::connect(&dicomReader, &DicomReader::slicesProcessed, &glviewer, &GLviewer::drawSlices);
-
-    static_cast<QWindow*>(appWindow)->show();
-
-    return a.exec();
->>>>>>> fb481e1f758942f5d9a396b881637484aff42b24
 }
